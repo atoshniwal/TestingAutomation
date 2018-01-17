d7e8caa64f8e3dade187077ac6c972f30e551d20
Arvind Toshniwal <arvind.toshniwal@redknee.com>
2018-01-17 18:24:27 +0530

    Adding AppTestJava Modifications

--- src/test/java/com/onlineshopping/AppTest.java
+++ src/test/java/com/onlineshopping/AppTest.java
@@ -18,7 +18,7 @@ public class AppTest
     public AppTest( String testName )
     {
         super( testName );
-        System.out.println("I am here");
+        //System.out.println("I am here");
     }
 
     /**
